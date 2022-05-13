bc2f6b6f651e7ba2e45634aedbc5dda0b64f2e71
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2022-05-13 16:39:29 +0200

    fixed scripts

--- update-docker-version.sh
+++ update-docker-version.sh
@@ -2,5 +2,5 @@
 
 docker build --no-cache -t "maxwai/adlist-merger:$1" .
 docker build -t maxwai/adlist-merger:latest .
-docker push "maxwai/sadlist-merger:$1"
+docker push "maxwai/adlist-merger:$1"
 docker push maxwai/adlist-merger:latest
