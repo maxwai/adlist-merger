8bae6cddba56b8254ce54cbc506d78c3e7980525
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2023-01-19 11:37:54 +0900

    changed what lines are not valid

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
@@ -49,9 +50,9 @@ public class Main {
 	);
 	
 	private static final List<Function<String, Boolean>> ILLEGAL_LINES = List.of(
-			s -> s.equals("localhost"),
-			s -> s.equals("fe"),
-			s -> s.equals("ff")
+			s -> s.equals("0.0.0.0"),
+			s -> !s.contains("."),
+			s -> !StandardCharsets.US_ASCII.newEncoder().canEncode(s)
 	);
 	
 	public static void main(String[] args) {
